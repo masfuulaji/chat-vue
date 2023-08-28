781db6aa9861e9c5505cfd18421aae656655ad97
masfuulaji <masfuulaji@gmail.com>
2023-08-28 23:22:35 +0700

    adjuxtment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
     <div data-theme="dracula" class="flex justify-center items-center h-screen">
+
         <RouterLink class="btn btn-primary mx-2" :to="{ name: 'chat', params: { room: 'one' } }">Room 1</RouterLink>
         <RouterLink class="btn btn-primary mx-2" :to="{ name: 'chat', params: { room: 'two' } }">Room 2</RouterLink>
     </div>
