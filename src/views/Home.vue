4eb9ce90c2f0f206d0132e7cf556fad53c889d0f
masfuulaji <masfuulaji@gmail.com>
2023-09-13 02:42:08 +0700

    add crud room

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,74 @@
 <template>
-    <div data-theme="dracula" class="flex justify-center items-center h-screen">
-
-        <RouterLink class="btn btn-primary mx-2" :to="{ name: 'chat', params: { room: 'one' } }">Room 1</RouterLink>
-        <RouterLink class="btn btn-primary mx-2" :to="{ name: 'chat', params: { room: 'two' } }">Room 2</RouterLink>
+    <div data-theme="dracula">
+        <div class="h-screen p-4">
+            <div class="flex justify-between items-center m-2">
+                <h1 class="text-5xl ml-2">Room</h1>
+                <div class="flex">
+                    <button class="btn btn-warning mr-2" @click="getRooms">Refresh</button>
+                    <button class="btn btn-success" @click="showModal = true">Add</button>
+                </div>
+            </div>
+            <div class="flex flex-col">
+                <div class="my-2 flex" v-for="room in rooms">
+                    <RouterLink :key="room.id" class="btn btn-info w-11/12"
+                        :to="{ name: 'chat', params: { room: room.id } }">{{ room.name }}</RouterLink>
+                    <button class="btn btn-error w-1/12" @click="deleteRoom(room.id)">Delete</button>
+                </div>
+            </div>
+        </div>
+        <dialog class="modal" :class="{ 'modal-open': showModal }">
+            <div class="modal-box">
+                <h3 class="font-bold text-lg mb-2">Create Room</h3>
+                <div class="form-control">
+                    <input type="text" v-model="room" placeholder="Room Name" class="input input-bordered" />
+                </div>
+                <div class="modal-action">
+                    <button @click="showModal = false" class="btn btn-neutral">Close</button>
+                    <button @click="saveRoom" class="btn btn-success">Create</button>
+                </div>
+            </div>
+            <form method="dialog" class="modal-backdrop">
+                <button @click="showModal = false">close</button>
+            </form>
+        </dialog>
     </div>
 </template>
 
-<script lang="ts">
-export default {
-    name: 'Home',
-    setup() {
-    },
-};
+<script setup lang="ts">
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+
+const showModal = ref(false);
+
+type Room = {
+    id: string;
+    name: string;
+}
+const rooms = ref([] as Room[]);
+const room = ref('');
+
+const getRooms = async () => {
+    const response = await axios.get('http://localhost:8080/room');
+    rooms.value = response.data;
+}
+
+const deleteRoom = async (id: string) => {
+    await axios.delete('http://localhost:8080/room/' + id);
+    getRooms();
+}
+
+const saveRoom = async () => {
+    await axios.post('http://localhost:8080/room', {
+        name: room.value
+    })
+    getRooms();
+
+    room.value = '';
+
+    showModal.value = false;
+}
+onMounted(() => {
+    getRooms();
+})
 </script>
 
