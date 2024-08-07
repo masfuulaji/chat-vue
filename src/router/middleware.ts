79ba9a002e9c6852f9e04b6217064ce7202c7199
masfuulaji <masfuulaji@gmail.com>
2024-08-07 09:38:39 +0700

    fix middleware

--- src/router/middleware.ts
+++ src/router/middleware.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function checkAuth({ to, from, next }: { to: any; from: any; next: any }) {
     try {
-        const response = await axios.get('http://localhost:8080/auth/check', { withCredentials: true })
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/auth/check`, { withCredentials: true })
         if (response.data.id) {
             localStorage.setItem('user_id', response.data.id)
         } else {
