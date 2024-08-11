74e950e1f846e2fa9a1c4b2964ee6c39d5af9147
masfuulaji <masfuulaji@gmail.com>
2024-08-12 06:45:47 +0700

    update css

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -27,7 +27,7 @@
                         <div class="mt-2">
                             <input id="password" v-model="formLogin.password" type="password"
                                 autocomplete="current-password" required
-                                class="block w-full rounded-md border-0 py-2 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                class="block w-full rounded-md border-0 py-2 px-2 text-gray-100 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
                     </div>
 
--- src/views/login/Register.vue
+++ src/views/login/Register.vue
@@ -29,7 +29,7 @@
                         <div class="mt-2">
                             <input id="password" v-model="formLogin.password" type="password"
                                 autocomplete="current-password" required
-                                class="block w-full rounded-md border-0 py-2 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                class="block w-full rounded-md border-0 py-2 px-2 text-gray-100 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
                     </div>
 
@@ -37,6 +37,8 @@
                         <button @click="login" type="button"
                             class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
                             Up</button>
+                        <button @click="RouteBack" type="button"
+                            class="mt-4 flex w-full justify-center rounded-md bg-purple-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Cancel</button>
                     </div>
                 </div>
             </div>
@@ -72,5 +74,8 @@ const login = async () => {
     } catch (error) {
         console.log(error);
     }
+}
+const RouteBack = () => {
+    router.push({ name: 'login' });
 }
 </script>
